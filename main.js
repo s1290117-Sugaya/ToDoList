d4c4f30336cd963fe653ed2faa070e3236653c90
s1290117-Sugaya <s1290117@u-aizu.ac.jp>
2021-07-15 21:50:43 +0900

    hoge

--- main.js
+++ main.js
@@ -3,20 +3,16 @@ const button = document.getElementById("button")
 const textarea = document.getElementById("textarea")
 
 function addTask(){
-	let i=0
 	const pElement = document.createElement("p")
 	const pButton = document.createElement("button")
 	pElement.textContent = textarea.value
-	pElement.id = i
-	pButton.value = "削除"
-	pButton.id = i++
 	main.appendChild(pElement)
 	main.appendChild(pButton)
 
-function rmTask(){
-        pElement.remove()
-        pButton.remove()
-}
+        function rmTask(){
+          pElement.remove()
+          pButton.remove()
+        }
 
         pButton.addEventListener("click", rmTask)
 }
